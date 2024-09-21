1554bb45c0c1552cefd39bdd15e55e8beedb0d8e
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-09-21 02:34:28 +0200

    Update route.ts

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -21,17 +21,13 @@ export async function POST(req: Request) {
   const body = await req.text();
   const signature = (headers().get("Stripe-Signature") ||
     headers().get("stripe-signature")) as string;
-  // const signature = stripe.webhooks.generateTestHeaderString({
-  //   payload: body,
-  //   secret: env.STRIPE_WEBHOOK_SECRET,
-  // });
+
   let event: Stripe.Event;
   try {
     event = stripe.webhooks.constructEvent(
       body,
       signature,
-      env.STRIPE_WEBHOOK_SECRET,
-      // env.STRIPE_WEBHOOK_SECRET,
+      env.STRIPE_WEBHOOK_SECRET
     );
   } catch (error) {
     console.log(`❌ Error message: ${error.message}`);
@@ -42,45 +38,49 @@ export async function POST(req: Request) {
 
   const session = event.data.object as Stripe.Checkout.Session;
 
-  // if (event.type === "checkout.session.completed") {
-  //   // Retrieve the subscription details from Stripe.
-  //   const subscription = await stripe.subscriptions.retrieve(
-  //     session.subscription as string,
-  //   );
+  if (event.type === "checkout.session.completed") {
+    // Retrieve the subscription details from Stripe.
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription as string
+    );
 
-  //   // Update the user stripe into in our database.
-  //   // Since this is the initial subscription, we need to update
-  //   // the subscription id and customer id.
-  //   await db
-  //     .update(userPaymentInfo)
-  //     .set({
-  //       stripeSubscriptionId: subscription.id,
-  //       stripeCustomerId: subscription.customer as string,
-  //       stripePriceId: subscription.items.data[0].price.id,
-  //       stripeCurrentPeriodEnd: new Date(
-  //         subscription.current_period_end * 1000,
-  //       ),
-  //     })
-  //     .where(eq(userPaymentInfo.userId, session?.metadata?.userId as string));
-  // }
+    // Update the user stripe info in our database.
+    await prisma.userPaymentInfo.update({
+      where: { id: parseInt(session?.metadata?.userId as string) },
+      data: {
+        stripeSubscriptionId: subscription.id,
+        stripeCustomerId: subscription.customer as string,
+        stripePriceId: subscription.items.data[0].price.id,
+        stripeCurrentPeriodEnd: new Date(
+          subscription.current_period_end * 1000
+        ),
+      },
+    });
+  }
 
-  // if (event.type === "invoice.payment_succeeded") {
-  //   // Retrieve the subscription details from Stripe.
-  //   const subscription = await stripe.subscriptions.retrieve(
-  //     session.subscription as string,
-  //   );
+  if (event.type === "invoice.payment_succeeded") {
+    // Retrieve the subscription details from Stripe.
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription as string
+    );
 
-  //   // Update the price id and set the new period end.
-  //   await db
-  //     .update(userPaymentInfo)
-  //     .set({
-  //       stripePriceId: subscription.items.data[0].price.id,
-  //       stripeCurrentPeriodEnd: new Date(
-  //         subscription.current_period_end * 1000,
-  //       ),
-  //     })
-  //     .where(eq(userPaymentInfo.stripeSubscriptionId, subscription.id));
-  // }
+    const userPaymentInfo = await prisma.userPaymentInfo.findFirst({
+      where: { stripeSubscriptionId: subscription.id },
+    });
+
+    if (userPaymentInfo) {
+      await prisma.userPaymentInfo.update({
+        where: { id: userPaymentInfo.id },
+        data: {
+          stripePriceId: subscription.items.data[0].price.id,
+          stripeCurrentPeriodEnd: new Date(
+            subscription.current_period_end * 1000
+          ),
+        },
+      });
+    }
+  }
+
   if (event.type === "payment_intent.payment_failed") {
     const metaOrderId = session?.metadata?.orderId as string;
     const [orderId] = ChargeOrderHashids.decode(metaOrderId);
