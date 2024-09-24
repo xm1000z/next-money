ff1de044da6eb8ce60e7fdcec66f09584fb2ae49
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-09-24 02:46:26 +0200

    Update animated-gradient-text.tsx

--- components/magicui/animated-gradient-text.tsx
+++ components/magicui/animated-gradient-text.tsx
@@ -12,15 +12,14 @@ export default function AnimatedGradientText({
   return (
     <div
       className={cn(
-        "group relative mx-auto flex max-w-fit flex-row items-center justify-center rounded-2xl bg-white/40 px-4 py-1.5 text-sm font-medium shadow-[inset_0_-8px_10px_#8fdfff1f] backdrop-blur-sm transition-shadow duration-500 ease-out [--bg-size:300%] hover:shadow-[inset_0_-5px_10px_#8fdfff3f] dark:bg-black/40",
+        "group relative mx-auto flex max-w-fit flex-row items-center justify-center rounded-2xl bg-white/40 px-4 py-1.5 text-sm font-medium shadow-[inset_0_-8px_10px_#8fdfff1f] backdrop-blur-sm transition-shadow duration-500 ease-out hover:shadow-[inset_0_-5px_10px_#8fdfff3f] dark:bg-black/40",
         className,
       )}
     >
       <div
-        className={`animate-gradient absolute inset-0 block h-full w-full bg-gradient-to-r from-[#afafaf] via-[#d6d6d6] to-[#ffffff] bg-[length:var(--bg-size)_100%] p-[1px] [border-radius:inherit] ![mask-composite:subtract] [mask:linear-gradient(#fff_0_0)_content-box,linear-gradient(#fff_0_0)]`}
+        className="absolute inset-0 rounded-2xl bg-gradient-to-r from-[#afafaf] via-[#d6d6d6] to-[#ffffff] p-[1px] animate-led-border"
       />
-
-      {children}
+      <div className="relative z-10">{children}</div>
     </div>
   );
 }
