fc70d4a929724e65ce96dd2806116172145cf4e3
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-09-26 14:07:07 +0200

    c

--- components/ui/modal.tsx
+++ components/ui/modal.tsx
@@ -4,7 +4,7 @@ import { Dispatch, SetStateAction } from "react";
 // import { useRouter } from "next/router";
 import { Drawer } from "vaul";
 
-import { Dialog, DialogContent } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
 import { useMediaQuery } from "@/hooks/use-media-query";
 import { cn } from "@/lib/utils";
 
