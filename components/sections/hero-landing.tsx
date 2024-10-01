6b481889c2c8f36a90e0d9c1ad7ae1289d422bb3
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-10-01 20:10:29 +0200

    messirve

--- components/sections/hero-landing.tsx
+++ components/sections/hero-landing.tsx
@@ -27,7 +27,7 @@ export default async function HeroLanding() {
             <span className="mr-3">🍓</span>
             <span
               className={cn(
-                `inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`,
+                `inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#ff4040] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`,
               )}
             >
               {t("intro")}
