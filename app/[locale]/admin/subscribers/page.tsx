263b3d243500ac207b23e4500bbe63c4a48115de
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-10-22 03:37:56 +0200

    f

--- app/[locale]/admin/subscribers/page.tsx
+++ app/[locale]/admin/subscribers/page.tsx
@@ -5,25 +5,19 @@ import { prisma } from "@/db/prisma";
 import SubscribersCard from "./_mods/card";
 
 export default async function AdminSubscribersPage() {
-  const count = await prisma.$queryRaw`
+  const countResult = await prisma.$queryRaw<[{
+    today_count: number;
+    this_month_count: number;
+    total: number;
+  }]>`
     SELECT
-      (SELECT COUNT(*) FROM subscribers WHERE subscribed_at::date = CURRENT_DATE) AS today_count,
+      (SELECT COUNT(*) FROM subscribers WHERE DATE(subscribed_at) = CURRENT_DATE) AS today_count,
       (SELECT COUNT(*) FROM subscribers WHERE EXTRACT(YEAR FROM subscribed_at) = EXTRACT(YEAR FROM CURRENT_DATE) AND EXTRACT(MONTH FROM subscribed_at) = EXTRACT(MONTH FROM CURRENT_DATE)) AS this_month_count,
       (SELECT COUNT(*) FROM subscribers WHERE subscribed_at IS NOT NULL) as total
   `;
 
-   const {
-     rows: [count],
-   } = await db.execute<{
-     total: number;
-     today_count: number;
-     this_month_count: number;
-   }>(
-     sql`SELECT
-   (SELECT COUNT(*) FROM subscribers WHERE subscribed_at::date = CURRENT_DATE) AS today_count,
-   (SELECT COUNT(*) FROM subscribers WHERE EXTRACT(YEAR FROM subscribed_at) = EXTRACT(YEAR FROM CURRENT_DATE) AND EXTRACT(MONTH FROM subscribed_at) = EXTRACT(MONTH FROM CURRENT_DATE)) AS this_month_count,
-   (SELECT COUNT(*) FROM subscribers WHERE subscribed_at IS NOT NULL) as total`,
-   );
+  const count = countResult[0] || { today_count: 0, this_month_count: 0, total: 0 };
+
   const subs = await prisma.subscribers.findMany({
     where: {
       subscribedAt: {
@@ -36,5 +30,5 @@ export default async function AdminSubscribersPage() {
     },
   });
 
-  return <SubscribersCard count={count ?? {}} dataSource={subs} />;
+  return <SubscribersCard count={count} dataSource={subs} />;
 }
