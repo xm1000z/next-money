cd718d3ccd8c2c41396cb26beb5608848820c1a9
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-10-24 00:47:38 +0200

    aa

--- components/magicui/animated-gradient-text.tsx
+++ components/magicui/animated-gradient-text.tsx
@@ -12,14 +12,10 @@ export default function AnimatedGradientText({
   return (
     <div
       className={cn(
-        "group relative mx-auto flex max-w-fit flex-row items-center justify-center rounded-2xl bg-white/40 px-4 py-1.5 text-sm font-medium shadow-[inset_0_-8px_10px_#8fdfff1f] backdrop-blur-sm transition-shadow duration-500 ease-out [--bg-size:300%] hover:shadow-[inset_0_-5px_10px_#8fdfff3f] dark:bg-black/40",
+        "justify-center text-sm font-medium transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 border bg-transparent hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2 rounded-full border-border flex space-x-2 items-center",
         className,
       )}
     >
-      <div
-        className={`animate-gradient absolute inset-0 block h-full w-full bg-gradient-to-r from-[#ffaa40]/50 via-[#ff4040]/50 to-[#ffaa40]/50 bg-[length:var(--bg-size)_100%] p-[1px] [border-radius:inherit] ![mask-composite:subtract] [mask:linear-gradient(#fff_0_0)_content-box,linear-gradient(#fff_0_0)]`}
-      />
-
       {children}
     </div>
   );
