1d3bd11524e821e0768ee44e2a453670b7fa5993
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-10-30 00:40:12 +0100

    ª

--- components/layout/dashboard-sidebar.tsx
+++ components/layout/dashboard-sidebar.tsx
@@ -61,69 +61,95 @@ export function DashboardSidebar({ links }: DashboardSidebarProps) {
   };
 
   return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger>
-          <div className="sticky top-0 h-full">
-            <ScrollArea className="h-full overflow-y-auto border-r">
-              <aside
-                className={cn(
-                  isSidebarExpanded ? "w-[220px] xl:w-[260px]" : "w-[68px]",
-                  "hidden h-screen md:block",
-                )}
-              >
-                <div className="flex h-full max-h-screen flex-1 flex-col gap-2">
-                  <div className="flex h-14 items-center p-4 lg:h-[60px]">
-                    {isSidebarExpanded ? <NavbarLogo size="md" /> : null}
+    <TooltipProvider delayDuration={0}>
+      <div className="sticky top-0 h-full">
+        <ScrollArea className="h-full overflow-y-auto border-r">
+          <aside
+            className={cn(
+              isSidebarExpanded ? "w-[220px] xl:w-[260px]" : "w-[68px]",
+              "hidden h-screen md:block",
+            )}
+          >
+            <div className="flex h-full max-h-screen flex-1 flex-col gap-2">
+              <div className="flex h-14 items-center p-4 lg:h-[60px]">
+                {isSidebarExpanded ? <NavbarLogo size="md" /> : null}
 
-                    <Button
-                      variant="ghost"
-                      size="icon"
-                      className="ml-auto size-9 lg:size-8"
-                      onClick={toggleSidebar}
-                    >
-                      {isSidebarExpanded ? (
-                        <PanelLeftClose
-                          size={18}
-                          className="stroke-muted-foreground"
-                        />
-                      ) : (
-                        <PanelRightClose
-                          size={18}
-                          className="stroke-muted-foreground"
-                        />
-                      )}
-                      <span className="sr-only">Toggle Sidebar</span>
-                    </Button>
-                  </div>
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  className="ml-auto size-9 lg:size-8"
+                  onClick={toggleSidebar}
+                >
+                  {isSidebarExpanded ? (
+                    <PanelLeftClose
+                      size={18}
+                      className="stroke-muted-foreground"
+                    />
+                  ) : (
+                    <PanelRightClose
+                      size={18}
+                      className="stroke-muted-foreground"
+                    />
+                  )}
+                  <span className="sr-only">Toggle Sidebar</span>
+                </Button>
+              </div>
 
-                  <nav className="flex flex-1 flex-col gap-8 px-4 pt-4">
-                    {links.map((section) => (
-                      <section
-                        key={section.title}
-                        className="flex flex-col gap-0.5"
-                      >
-                        {isSidebarExpanded ? (
-                          <p className="text-xs text-muted-foreground">
-                            {section.title}
-                          </p>
-                        ) : (
-                          <div className="h-4" />
-                        )}
-                        {section?.items?.map((item) => {
-                          const Icon = Icons[item.icon || "arrowRight"];
-                          const RightIcon = item.rightIcon ? Icons[item.rightIcon] : null;
-                          return (
-                            item.href && (
-                              <Fragment key={`link-fragment-${item.title}`}>
-                                {isSidebarExpanded ? (
+              <nav className="flex flex-1 flex-col gap-8 px-4 pt-4">
+                {links.map((section) => (
+                  <section
+                    key={section.title}
+                    className="flex flex-col gap-0.5"
+                  >
+                    {isSidebarExpanded ? (
+                      <p className="text-xs text-muted-foreground">
+                        {section.title}
+                      </p>
+                    ) : (
+                      <div className="h-4" />
+                    )}
+                    {section?.items?.map((item) => {
+                      const Icon = Icons[item.icon || "arrowRight"];
+                      const RightIcon = item.rightIcon ? Icons[item.rightIcon] : null;
+                      return (
+                        item.href && (
+                          <Fragment key={`link-fragment-${item.title}`}>
+                            {isSidebarExpanded ? (
+                              <Link
+                                key={`link-${item.title}`}
+                                href={item.disabled ? "#" : item.href}
+                                target={item.external ? "_blank" : undefined}
+                                rel={item.external ? "noopener noreferrer" : undefined}
+                                className={cn(
+                                  "flex items-center gap-3 rounded-md p-2 text-sm font-medium hover:bg-muted",
+                                  path === item.href
+                                    ? "bg-muted"
+                                    : "text-muted-foreground hover:text-accent-foreground",
+                                  item.disabled &&
+                                    "cursor-not-allowed opacity-80 hover:bg-transparent hover:text-muted-foreground",
+                                )}
+                              >
+                                <Icon className="size-5" />
+                                <span className="flex-grow">
+                                  {t(item.title, { fallback: item.title })}
+                                </span>
+                                {item.badge && (
+                                  <Badge className="ml-auto flex size-5 shrink-0 items-center justify-center rounded-full">
+                                    {item.badge}
+                                  </Badge>
+                                )}
+                                {item.external && <Icons.externalLink className="size-4 ml-2" />}
+                              </Link>
+                            ) : (
+                              <Tooltip key={`tooltip-${item.title}`}>
+                                <TooltipTrigger asChild>
                                   <Link
-                                    key={`link-${item.title}`}
+                                    key={`link-tooltip-${item.title}`}
                                     href={item.disabled ? "#" : item.href}
                                     target={item.external ? "_blank" : undefined}
                                     rel={item.external ? "noopener noreferrer" : undefined}
                                     className={cn(
-                                      "flex items-center gap-3 rounded-md p-2 text-sm font-medium hover:bg-muted",
+                                      "flex items-center gap-3 rounded-md py-2 text-sm font-medium hover:bg-muted",
                                       path === item.href
                                         ? "bg-muted"
                                         : "text-muted-foreground hover:text-accent-foreground",
@@ -131,64 +157,31 @@ export function DashboardSidebar({ links }: DashboardSidebarProps) {
                                         "cursor-not-allowed opacity-80 hover:bg-transparent hover:text-muted-foreground",
                                     )}
                                   >
-                                    <Icon className="size-5" />
-                                    <span className="flex-grow">
-                                      {t(item.title, { fallback: item.title })}
+                                    <span className="flex size-full items-center justify-center">
+                                      <Icon className="size-5" />
                                     </span>
-                                    {item.badge && (
-                                      <Badge className="ml-auto flex size-5 shrink-0 items-center justify-center rounded-full">
-                                        {item.badge}
-                                      </Badge>
-                                    )}
-                                    {item.external && <Icons.externalLink className="size-4 ml-2" />}
                                   </Link>
-                                ) : (
-                                  <Tooltip key={`tooltip-${item.title}`}>
-                                    <TooltipTrigger asChild>
-                                      <Link
-                                        key={`link-tooltip-${item.title}`}
-                                        href={item.disabled ? "#" : item.href}
-                                        target={item.external ? "_blank" : undefined}
-                                        rel={item.external ? "noopener noreferrer" : undefined}
-                                        className={cn(
-                                          "flex items-center gap-3 rounded-md py-2 text-sm font-medium hover:bg-muted",
-                                          path === item.href
-                                            ? "bg-muted"
-                                            : "text-muted-foreground hover:text-accent-foreground",
-                                          item.disabled &&
-                                            "cursor-not-allowed opacity-80 hover:bg-transparent hover:text-muted-foreground",
-                                        )}
-                                      >
-                                        <span className="flex size-full items-center justify-center">
-                                          <Icon className="size-5" />
-                                        </span>
-                                      </Link>
-                                    </TooltipTrigger>
-                                    <TooltipContent side="right">
-                                      {t(item.title)}
-                                    </TooltipContent>
-                                  </Tooltip>
-                                )}
-                              </Fragment>
-                            )
-                          );
-                        })}
-                      </section>
-                    ))}
-                  </nav>
+                                </TooltipTrigger>
+                                <TooltipContent side="right">
+                                  {t(item.title)}
+                                </TooltipContent>
+                              </Tooltip>
+                            )}
+                          </Fragment>
+                        )
+                      );
+                    })}
+                  </section>
+                ))}
+              </nav>
 
-                  <div className="mt-auto xl:p-4">
-                    {/* {isSidebarExpanded ? <UpgradeCard /> : null} */}
-                  </div>
-                </div>
-              </aside>
-            </ScrollArea>
-          </div>
-        </TooltipTrigger>
-        <TooltipContent>
-          {/* Tu contenido del tooltip */}
-        </TooltipContent>
-      </Tooltip>
+              <div className="mt-auto xl:p-4">
+                {/* {isSidebarExpanded ? <UpgradeCard /> : null} */}
+              </div>
+            </div>
+          </aside>
+        </ScrollArea>
+      </div>
     </TooltipProvider>
   );
 }
