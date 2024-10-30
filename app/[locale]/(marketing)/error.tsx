42da7ceb7d9649d926e621a3e4abede845560b3b
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-10-30 14:37:10 +0100

    aa

--- app/[locale]/(marketing)/error.tsx
+++ app/[locale]/(marketing)/error.tsx
@@ -10,7 +10,7 @@ export default function Error({
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
-      <h2 className="mb-5 text-center">Something went wrong!</h2>
+      <h2 className="mb-5 text-center">Notas que algo ha fallado?</h2>
       <Button
         type="submit"
         variant="default"
