b209bc6bf959e180aca5c5a300503a766c6f9571
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-10-30 11:53:27 +0100

    a

--- components/user-info.tsx
+++ components/user-info.tsx
@@ -88,16 +88,14 @@ export function UserInfo() {
           >
             <TooltipProvider>
               <Tooltip>
-                  <TooltipTrigger asChild>
-                    <button
-                      onClick={() => window.location.href = '/sign-in'}
-                      type="button"
-                      className="group h-10 bg-gradient-to-b from-zinc-50/50 to-white/90 px-3 text-sm shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:from-zinc-900/50 dark:to-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20"
-                    >
-                      <UserArrowLeftIcon className="h-5 w-5" />
-                    </button>..
-                  </TooltipTrigger>
-
+                <TooltipTrigger asChild>
+                  <button
+                    onClick={() => window.location.href = '/sign-in'}
+                    className="group h-10 bg-gradient-to-b from-zinc-50/50 to-white/90 px-3 text-sm shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:from-zinc-900/50 dark:to-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20"
+                  >
+                    <UserArrowLeftIcon className="h-5 w-5" />
+                  </button>
+                </TooltipTrigger>
                 <TooltipContent>
                   <motion.div
                     initial={{ opacity: 0, scale: 0.96 }}
