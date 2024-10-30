dc95f6a5f0aae233c2f2f15059c3fa361ab06af0
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-10-30 14:15:01 +0100

    a

--- components/user-info.tsx
+++ components/user-info.tsx
@@ -57,13 +57,13 @@ export function UserInfo() {
           {!pathname?.includes("app") && (
             <TooltipProvider>
               <Tooltip>
+              <TooltipTrigger asChild>
                 <Link href="/app" className="size-full">
-                  <TooltipTrigger asChild>
                     <button className="group h-10 bg-gradient-to-b from-zinc-50/50 to-white/90 px-3 text-sm shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:from-zinc-900/50 dark:to-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20">
                       <UserArrowLeftIcon className="h-5 w-5" />
                     </button>
-                  </TooltipTrigger>
                 </Link>
+                </TooltipTrigger>
                 <TooltipContent>
                   <motion.div
                     initial={{ opacity: 0, scale: 0.96 }}
@@ -88,16 +88,16 @@ export function UserInfo() {
           >
             <TooltipProvider>
               <Tooltip>
+              <TooltipTrigger asChild>
                 <Link href="/sign-in" className="size-full">
-                  <TooltipTrigger asChild>
                     <button
                       type="button"
                       className="group h-10 bg-gradient-to-b from-zinc-50/50 to-white/90 px-3 text-sm shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:from-zinc-900/50 dark:to-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20"
                     >
                       <UserArrowLeftIcon className="h-5 w-5" />
                     </button>
-                  </TooltipTrigger>
                 </Link>
+                </TooltipTrigger>
 
                 <TooltipContent>
                   <motion.div
