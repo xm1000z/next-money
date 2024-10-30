b43caa517513261811a8173e4178b8816189999b
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-10-30 23:52:35 +0100

    a

--- components/ui/sign-in-form.tsx
+++ components/ui/sign-in-form.tsx
@@ -18,7 +18,8 @@ export default function SignInForm() {
       header: 'hidden',
       formFieldLabelRow : 'hidden',
       buttonArrowIcon: 'hidden',
-      formFieldInput: 'flex h-9 w-full border !border-[#2b2b2b] bg-transparent px-3 py-1 text-sm text-black dark:text-white transition-colors file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
+      formFieldInput: 'flex h-9 w-full border !border-[#2b2b2b] bg-transparent px-3 py-1 text-sm text-black dark:text-[#2b2b2b] transition-colors file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
+
           formButtonPrimary: 'h-10 w-full bg-black text-md text-white hover:bg-zinc-900 dark:bg-white dark:text-black dark:hover:bg-[#FFFFF] font-bold focus-visible:outline-none disabled:pointer-events-none',
       footerAction: 'hidden', // Oculta elementos del footer
       footer: 'hidden' // Oculta el footer completo
@@ -37,7 +38,7 @@ export default function SignInForm() {
   );
 
   return (
-    <div className="dark:bg-[#121212]">
+    <div className="dark bg-[#121212]">
       <header className="w-full fixed left-0 right-0">
         <div className="ml-5 mt-4 md:ml-10 md:mt-10">
           <Link href="https://app.notas.ai">
