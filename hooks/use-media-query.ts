da3850d52f62f17c731a5aaf70f71787efdb4559
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-11-05 14:01:20 +0100

    a

--- hooks/use-media-query.ts
+++ hooks/use-media-query.ts
@@ -1,49 +1,25 @@
 import { useEffect, useState } from "react";
 
-export function useMediaQuery() {
-  const [device, setDevice] = useState<
-    "mobile" | "sm" | "tablet" | "desktop" | null
-  >(null);
-  const [dimensions, setDimensions] = useState<{
-    width: number;
-    height: number;
-  } | null>(null);
+export function useMediaQuery(query: string): boolean {
+  const [matches, setMatches] = useState(false);
 
   useEffect(() => {
-    const checkDevice = () => {
-      if (window.matchMedia("(max-width: 640px)").matches) {
-        setDevice("mobile");
-      } else if (window.matchMedia("(max-width: 768px)").matches) {
-        setDevice("sm");
-      } else if (
-        window.matchMedia("(min-width: 641px) and (max-width: 1024px)").matches
-      ) {
-        setDevice("tablet");
-      } else {
-        setDevice("desktop");
-      }
-      setDimensions({ width: window.innerWidth, height: window.innerHeight });
+    const media = window.matchMedia(query);
+    
+    // Establecer el estado inicial
+    setMatches(media.matches);
+
+    // Definir el callback
+    const listener = (event: MediaQueryListEvent) => {
+      setMatches(event.matches);
     };
 
-    // Initial detection
-    checkDevice();
+    // Añadir el listener
+    media.addEventListener("change", listener);
 
-    // Listener for windows resize
-    window.addEventListener("resize", checkDevice);
+    // Cleanup
+    return () => media.removeEventListener("change", listener);
+  }, [query]);
 
-    // Cleanup listener
-    return () => {
-      window.removeEventListener("resize", checkDevice);
-    };
-  }, []);
-
-  return {
-    device,
-    width: dimensions?.width,
-    height: dimensions?.height,
-    isMobile: device === "mobile",
-    isSm: device === "sm",
-    isTablet: device === "tablet",
-    isDesktop: device === "desktop",
-  };
+  return matches;
 }
