c618dcde9fdeb13766130fe9e05b605c58c6f8fa
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-11-05 16:27:28 +0100

    aa

--- components/card-stack.tsx
+++ components/card-stack.tsx
@@ -70,22 +70,8 @@ export const CardStack = ({
 
   return (
     <div
-      className={cn(
-        "relative z-10",
-        "before:absolute before:inset-0 before:-z-10 before:rounded-lg",
-        "before:bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] dark:before:bg-[radial-gradient(#1f2937_1px,transparent_1px)]",
-        "before:bg-[size:16px_16px]",
-        "before:opacity-50",
-        "h-[220px] w-[331px] md:h-[670px] md:w-[1031px]",
-        "bg-background/50 dark:bg-background/30",
-        "backdrop-blur-sm"
-      )}
-      onMouseEnter={() => {
-        if (intervalRef.current) {
-          clearInterval(intervalRef.current);
-        }
-      }}
-      onMouseLeave={() => !isAnimating && startFlipping()}
+      className="relative h-[220px] md:h-[670px] w-[331px] md:w-[1031px] z-10"
+      onMouseEnter={() => clearInterval(interval)}
     >
       {cards.map((card, index) => {
         return (
