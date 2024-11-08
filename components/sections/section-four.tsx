011b9595b96d6b45dc9cc220ac6ed3d766d1939b
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-11-08 02:53:54 +0100

    aaa

--- components/sections/section-four.tsx
+++ components/sections/section-four.tsx
@@ -5,14 +5,16 @@ import { CopyInput } from "@/components/copy-input";
 export function SectionFour() {
   return (
     <section className="flex justify-between space-y-12 md:space-y-0 md:space-x-8 flex-col md:flex-row overflow-hidden mb-12 mx-4 md:container md:px-0">
-      <div className="border border-border basis-1/3 bg-white dark:bg-[#121212] p-6 md:p-8 flex flex-col items-center justify-center">
-      <span className="text-[#F5F5F3] border border-border rounded-full self-start font-medium font-mono px-3 text-[10px] py-1.5 mb-3 bg-[#1D1D1D]">
+      <div className="border border-border basis-1/3 bg-white dark:bg-[#121212] p-6 md:p-8 flex flex-col">
+        <span className="text-[#F5F5F3] border border-border rounded-full self-start font-medium font-mono px-3 text-[10px] py-1.5 mb-3 bg-[#1D1D1D]">
           Muy pronto
         </span>
+        <div className="flex flex-col items-center justify-center flex-1">
         <h4 className="font-medium text-lg md:text-xl mb-2 text-gray-900 dark:text-white">Lisa</h4>
-        <p className="text-gray-600 dark:text-[#878787] text-base md:text-lg text-center">
-          Estamos trabajando arduamente para brindarte la mejor solución de audio con modelos web y conversación en vivo.
-        </p>
+          <p className="text-gray-600 dark:text-[#878787] text-base md:text-lg text-center">
+            Estamos trabajando arduamente para brindarte la mejor solución de audio con modelos web y conversación en vivo.
+          </p>
+        </div>
       </div>
 
       <div className="border border-border md:basis-2/3 bg-white dark:bg-[#121212] p-6 md:p-8 flex justify-between md:space-x-8 md:flex-row flex-col">
