4c96ab46aae287ff6e3e8e40faad74fc6ae9f0b2
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-11-11 02:08:34 +0100

    fgb

--- app/[locale]/(marketing)/layout.tsx
+++ app/[locale]/(marketing)/layout.tsx
@@ -1,7 +1,7 @@
 import { unstable_setRequestLocale } from "next-intl/server";
 
 import { NavMobile } from "@/components/layout/mobile-nav";
-import { NavBar } from "@/components/layout/navbar";
+import { NavBar } from "@/components/header";
 import { SiteFooter } from "@/components/layout/site-footer";
 import Promotion from "@/components/sections/promotion";
 
@@ -19,7 +19,7 @@ export default function MarketingLayout({
   return (
     <div className="flex min-h-screen flex-col">
       <NavMobile />
-      <NavBar scroll={true} />
+      <Header scroll={true} />
       <main className="flex-1">{children}</main>
       <SiteFooter />
       <Promotion locale={params.locale} />
--- components/sections/section-seven.tsx
+++ components/sections/section-seven.tsx
@@ -3,7 +3,7 @@ import { TextGenerateEffect } from "@/components/text-generate-effect";
 export function SectionSeven() {
   return (
     <TextGenerateEffect
-      className="md:pt-28 pt-12 pb-12 md:pb-32 text-4xl md:text-6xl max-w-[1370px] container md:leading-[85px] mb-12 text-gray-900 dark:text-white"
+      className="md:pt-28 pt-12 pb-12 md:pb-32 text-4xl md:text-4xl max-w-[1370px] container md:leading-[85px] mb-12 text-gray-900 dark:text-white"
       words="Activar el piloto automático pronto no será una opción sino una necesidad competitiva. NotasAI te ayuda a optimizar tus procesos, reducir el trabajo manual y obtener soluciones fácilmente gracias a la Inteligencia Artificial."
     />
   );
