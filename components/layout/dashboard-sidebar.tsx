35c4010104a8cacad1f03ff209f340f665c1e324
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-11-21 00:35:41 +0100

    d

--- components/layout/dashboard-sidebar.tsx
+++ components/layout/dashboard-sidebar.tsx
@@ -149,7 +149,7 @@ export function DashboardSidebar({ links }: DashboardSidebarProps) {
                                     target={item.external ? "_blank" : undefined}
                                     rel={item.external ? "noopener noreferrer" : undefined}
                                     className={cn(
-                                      "flex items-center gap-3 rounded-md py-2 text-sm font-medium hover:bg-muted",
+                                      "flex items-center gap-3 py-2 text-sm font-medium hover:bg-muted border border-[rgba(27, 27, 27, 0.18)] dark:border-[rgba(185, 185, 185, 0.17)] bg-[#ececec] dark:bg-[#1b1b1b]",
                                       path === item.href
                                         ? "bg-muted"
                                         : "text-muted-foreground hover:text-accent-foreground",
