0b32f416561568420d0d448ad998358156fa40bc
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-11-21 17:54:46 +0100

    as

--- db/queries/totalCredits.ts
+++
@@ -1,8 +0,0 @@
-import { prisma } from "@/db/prisma";
-
-export async function getTotalCredits(userId: string) {
-  const userCredits = await prisma.userCredit.findFirst({
-    where: { userId },
-  });
-  return userCredits ? userCredits.credit : 0; // Retornar el total de créditos
-} 
