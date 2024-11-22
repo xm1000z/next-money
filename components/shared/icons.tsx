2a268711de70bcc04585a39d59c49f4a3b4c9a44
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-11-22 13:21:46 +0100

    aaa

--- components/shared/icons.tsx
+++ components/shared/icons.tsx
@@ -14,6 +14,7 @@ import {
   HelpCircle,
   History,
   HomeIcon,
+  LayoutGrid,
   Image,
   Languages,
   Laptop,
@@ -69,6 +70,7 @@ export const Icons = {
   History,
   Languages,
   HomeIcon,
+  LayoutGrid,
   gitHub: ({ ...props }: LucideProps) => (
     <svg
       aria-hidden="true"
