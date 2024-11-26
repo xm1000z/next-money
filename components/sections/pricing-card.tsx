8d0959c73bd69180e91d2b413c5335431c27bb6f
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-11-26 23:20:01 +0100

    aaarf43q

--- components/sections/pricing-card.tsx
+++ components/sections/pricing-card.tsx
@@ -1,3 +1,4 @@
+import { auth } from "@clerk/nextjs/server";
 import { PricingCards } from "@/components/pricing-cards";
 import { PricingFaq } from "@/components/pricing-faq";
 import { getChargeProduct } from "@/db/queries/charge-product";
@@ -8,11 +9,16 @@ type Props = {
 };
 
 export default async function PricingCard(props: Props) {
+  const { userId } = auth();
   const { data: chargeProduct } = await getChargeProduct(props.locale);
 
   return (
     <div className="flex w-full flex-col gap-16 py-8 md:py-8">
-      <PricingCards chargeProduct={chargeProduct as any[]} subscriptionPlans={subscriptionPlans} />
+      <PricingCards 
+        chargeProduct={chargeProduct as any[]} 
+        subscriptionPlans={subscriptionPlans}
+        userId={userId}
+      />
       <hr className="container" />
       <PricingFaq />
     </div>
