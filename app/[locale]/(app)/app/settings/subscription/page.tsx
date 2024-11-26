e7286e8e2ba4734f8513e882753ba251f9a77835
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-11-26 20:45:33 +0100

    a

--- app/[locale]/(app)/app/settings/subscription/page.tsx
+++ app/[locale]/(app)/app/settings/subscription/page.tsx
@@ -1,4 +1,4 @@
-import { auth } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import { getUserSubscriptionPlan } from "@/lib/subscription";
 import { DashboardHeader } from "@/components/dashboard/header";
 import { DashboardShell } from "@/components/dashboard/shell";
