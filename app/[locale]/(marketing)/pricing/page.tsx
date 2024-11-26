c64e80bce29d49c0ec6dcaee2e77a66e63c1cc6c
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-11-26 23:02:25 +0100

    a

--- app/[locale]/(marketing)/pricing/page.tsx
+++ app/[locale]/(marketing)/pricing/page.tsx
@@ -1,6 +1,7 @@
 import { getTranslations, unstable_setRequestLocale } from "next-intl/server";
 
 import { PricingCards } from "@/components/pricing-cards";
+import { PricingFaq } from "@/components/pricing-faq";
 import { getChargeProduct } from "@/db/queries/charge-product";
 import { subscriptionPlans } from "@/config/subscription-plans";
 
@@ -11,7 +12,7 @@ type Props = {
 export async function generateMetadata({ params: { locale } }: Props) {
   const t = await getTranslations({ locale });
   return {
-    title: `Packs - NotasAI`,
+    title: `Precios`,
     description: t("LocaleLayout.description"),
   };
 }
@@ -25,9 +26,10 @@ export default async function PricingPage({ params: { locale } }: Props) {
     <div className="flex w-full flex-col gap-16 py-8 md:py-8">
       <PricingCards 
         chargeProduct={chargeProduct} 
-        subscriptionPlans={plans} 
+        subscriptionPlans={subscriptionPlans} 
       />
       <hr className="container" />
+      <PricingFaq />
     </div>
   );
 }
