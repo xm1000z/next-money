632646338769bef28248c6e8cbe8ab2c49bafa72
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-11-26 22:31:46 +0100

    a

--- components/sections/pricing-card.tsx
+++ components/sections/pricing-card.tsx
@@ -1,27 +1,17 @@
 import { PricingCards } from "@/components/pricing-cards";
 import { PricingFaq } from "@/components/pricing-faq";
-import { subscriptionPlans } from "@/config/subscription-plans";
 import { getChargeProduct } from "@/db/queries/charge-product";
 
-interface PricingCardProps {
+type Props = {
   locale: string;
-}
+};
 
-export default async function PricingCard({ locale }: PricingCardProps) {
-  const { data: chargeProduct = [] } = await getChargeProduct(locale);
-  const plans = subscriptionPlans.map(plan => ({
-    ...plan,
-    stripePriceIds: {
-      monthly: plan.stripePriceIds.monthly,
-      yearly: plan.stripePriceIds.yearly
-    }
-  }));
+export default async function PricingCard(props: Props) {
+  const { data: chargeProduct } = await getChargeProduct(props.locale);
 
   return (
     <div className="flex w-full flex-col gap-16 py-8 md:py-8">
-      <PricingCards 
-        chargeProduct={chargeProduct} 
-      />
+      <PricingCards chargeProduct={chargeProduct as any[]} />
       <hr className="container" />
       <PricingFaq />
     </div>
