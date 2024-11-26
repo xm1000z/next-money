a291ea60c3571b8dd872349ba041f64ea7177e85
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-11-26 22:54:25 +0100

    a

--- app/[locale]/(marketing)/pricing/page.tsx
+++ app/[locale]/(marketing)/pricing/page.tsx
@@ -1,8 +1,8 @@
 import { getTranslations, unstable_setRequestLocale } from "next-intl/server";
 
 import { PricingCards } from "@/components/pricing-cards";
-import { PricingFaq } from "@/components/pricing-faq";
 import { getChargeProduct } from "@/db/queries/charge-product";
+import { subscriptionPlans } from "@/config/subscription-plans";
 
 type Props = {
   params: { locale: string };
@@ -23,9 +23,11 @@ export default async function PricingPage({ params: { locale } }: Props) {
 
   return (
     <div className="flex w-full flex-col gap-16 py-8 md:py-8">
-      <PricingCards chargeProduct={chargeProduct} />
+      <PricingCards 
+        chargeProduct={chargeProduct} 
+        subscriptionPlans={plans} 
+      />
       <hr className="container" />
-      <PricingFaq />
     </div>
   );
 }
