fd842b2a833d8f2a9505eee9a53c86d068d99953
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-11-27 13:18:38 +0100

    aa

--- components/sections/pricing-card.tsx
+++ components/sections/pricing-card.tsx
@@ -8,7 +8,7 @@ interface PricingCardProps {
   locale: string;
 }
 
-export default async function PricingCard({ locale }: PricingCardProps) {
+export default async function PricingCard({ locale, userId }: PricingCardProps) {
   const { data: chargeProduct = [] } = await getChargeProduct(locale);
 
   const plans = subscriptionPlans.map(plan => ({
