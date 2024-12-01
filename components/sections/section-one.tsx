029dcf4f86c68d4a8fee481c42dcb5e4000a198d
Marc Furriols <74680137+xm1000z@users.noreply.github.com>
2024-12-01 15:14:44 +0100

    adf

--- components/sections/section-one.tsx
+++ components/sections/section-one.tsx
@@ -3,8 +3,7 @@ export function SectionOne() {
       <section className="mt-16 md:mt-32 mb-8 mx-4 md:container md:px-0">
         <h3 className="text-3xl md:text-5xl font-medium dark:text-white">Absolutamente todo</h3>
         <p className="mt-2 md:mt-4 text-sm text-[#878787] dark:text-gray-400">
-          Desde el mapeo automático de recibos a transacciones hasta la conversación con tus
-          finanzas y la consolidación de todos tus archivos
+          Desde un chat completo con todas las necesidades cubiertas hasta un buscador con Inteligencia Artificial.
         </p>
       </section>
     );
