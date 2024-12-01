ba13347a61442daa934e8fa5ca77ef5a839f365e
Marc Furriols <74680137+xm1000z@users.noreply.github.com>
2024-12-01 15:13:31 +0100

    asd

--- components/sections/section-one.tsx
+++ components/sections/section-one.tsx
@@ -1,7 +1,7 @@
 export function SectionOne() {
     return (
       <section className="mt-16 md:mt-32 mb-8 mx-4 md:container md:px-0">
-        <h3 className="text-2xl md:text-5xl font-medium dark:text-white">Todo lo que necesitas</h3>
+        <h3 className="text-3xl md:text-5xl font-medium dark:text-white">Absolutamente todo</h3>
         <p className="mt-2 md:mt-4 text-sm text-[#878787] dark:text-gray-400">
           Desde el mapeo automático de recibos a transacciones hasta la conversación con tus
           finanzas y la consolidación de todos tus archivos
