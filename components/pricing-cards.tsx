06742154d7bc20e6ea96a89e7d15a7a6d1de8c5e
Marc Furriols <74680137+xm1000z@users.noreply.github.com>
2024-12-02 20:51:12 +0100

    d

--- components/pricing-cards.tsx
+++ components/pricing-cards.tsx
@@ -266,7 +266,7 @@ export function PricingCards({
 
                   <div className="flex flex-col items-start">
                     <div className="flex items-baseline space-x-2 text-4xl font-semibold">
-                      {formatPrice(isYearly ? plan.price : plan.price, "€")}
+                      {formatPrice(plan.price, "€")}
                       <div className="text-base font-medium text-muted-foreground">
                         / {isYearly ? "año" : "mes"}
                       </div>
