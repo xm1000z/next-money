34191abaed8178ed8d48be9878e53567aa8ecb69
Marc Furriols <74680137+xm1000z@users.noreply.github.com>
2024-12-02 19:33:53 +0100

    aa

--- components/billing/index.tsx
+++ components/billing/index.tsx
@@ -6,7 +6,6 @@ import { useQueryUser } from '@/hooks/user-queries'
 type Props = {}
 
 const Billing = (props: Props) => {
-  const { data } = useQueryUser()
   return (
     <div className="flex lg:flex-row flex-col gap-5 w-full lg:w-10/12 xl:w-8/12 container">
       <PaymentCard
