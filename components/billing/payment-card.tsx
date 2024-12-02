bfec0a1c4e745af47642e50f30fb22f7d052922a
Marc Furriols <74680137+xm1000z@users.noreply.github.com>
2024-12-02 19:27:36 +0100

    afdsg

--- components/billing/payment-card.tsx
+++ components/billing/payment-card.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@/components/ui/button'
-import { PLANS } from '@/constants/pages'
+import { subscriptionPlans } from "@/config/constants";
 import { cn } from '@/lib/utils'
 import { CircleCheck } from 'lucide-react'
 import React from 'react'
@@ -58,7 +58,7 @@ const PaymentCard = ({ current, label, landing }: Props) => {
           <p className="text-xl mb-2">Free</p>
         )}
 
-        {PLANS[label === 'PRO' ? 1 : 0].features.map((i) => (
+        {subscriptionPlans[label === 'PRO' ? 1 : 0].features.map((i) => (
           <p
             key={i}
             className="mt-2 text-muted-foreground flex gap-2 "
