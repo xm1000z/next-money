f9c66093af818bc81361d51c8667690d33fcfb62
Marc Furriols <74680137+xm1000z@users.noreply.github.com>
2024-12-03 03:58:32 +0100

    c

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -3,6 +3,7 @@ import { InfiniteMovingCards } from "@/components/infinite-moving-cards";
 const testimonials = [
   {
     name: "Lucas Grey",
+    title: "Customer",
     avatarUrl:
       "https://pbs.twimg.com/profile_images/1843079229073981440/pQqZJX5G_400x400.jpg",
     handle: "@ImLucasGrey",
@@ -11,6 +12,7 @@ const testimonials = [
   },
   {
     name: "Patrick Tobler",
+    title: "User",
     avatarUrl:
       "https://pbs.twimg.com/profile_images/1821352126406127616/We8itUSn_400x400.jpg",
     handle: "@Padierfind",
@@ -19,6 +21,7 @@ const testimonials = [
   },
   {
     name: "Ben Tossell",
+    title: "Entrepreneur",
     avatarUrl:
       "https://pbs.twimg.com/profile_images/1595060668897677314/pHMUc1Zb_400x400.jpg",
     handle: "@bentossell",
@@ -28,6 +31,7 @@ const testimonials = [
   },
   {
     name: "Christian Alares",
+    title: "Developer",
     avatarUrl:
       "https://pbs.twimg.com/profile_images/1194368464946974728/1D2biimN_400x400.jpg",
     handle: "@c_alares",
@@ -36,6 +40,7 @@ const testimonials = [
   },
   {
     name: "Zeno Rocha",
+    title: "Designer",
     avatarUrl:
       "https://pbs.twimg.com/profile_images/1792735373887696896/Nys5Q2b3_400x400.jpg",
     handle: "@zenorocha",
@@ -44,6 +49,7 @@ const testimonials = [
   },
   {
     name: "Bailey Simrell",
+    title: "Content Creator",
     avatarUrl:
       "https://pbs.twimg.com/profile_images/1488962358609330178/tdTC7o6M_400x400.jpg",
     handle: "@baileysimrell",
@@ -52,6 +58,7 @@ const testimonials = [
   },
   {
     name: "Darshan Gajara",
+    title: "Analyst",
     handle: "@WeirdoWizard",
     verified: false,
     quote: "No sweat! Your smooth integration with banking data blew me away.",
@@ -60,6 +67,7 @@ const testimonials = [
   },
   {
     name: "Cal.com",
+    title: "Startup",
     avatarUrl:
       "https://pbs.twimg.com/profile_images/1839412200760610816/Lce29ADc_400x400.jpg",
     handle: "@calcom",
@@ -68,6 +76,7 @@ const testimonials = [
   },
   {
     name: "Guillermo Rauch",
+    title: "CEO",
     avatarUrl:
       "https://pbs.twimg.com/profile_images/1783856060249595904/8TfcCN0r_400x400.jpg",
     handle: "@rauchg",
@@ -77,6 +86,7 @@ const testimonials = [
   },
   {
     name: "Kyle @ KyTech",
+    title: "Tech Enthusiast",
     avatarUrl:
       "https://pbs.twimg.com/profile_images/1586538348964978689/nkpJWZxG_400x400.png",
     handle: "@KyTechInc",
@@ -85,6 +95,7 @@ const testimonials = [
   },
   {
     name: "Steven Tey",
+    title: "Entrepreneur",
     avatarUrl:
       "https://pbs.twimg.com/profile_images/1506792347840888834/dS-r50Je_400x400.jpg",
     handle: "@steventey",
@@ -101,6 +112,7 @@ const testimonials = [
   },
   {
     name: "Gokul",
+    title: "Developer",
     avatarUrl:
       "https://pbs.twimg.com/profile_images/1805103400549318656/EEQpiO7e_400x400.jpg",
     handle: "@KyTechInc",
@@ -109,6 +121,7 @@ const testimonials = [
   },
   {
     name: "Peer Richelsen — oss/acc",
+    title: "Founder",
     avatarUrl:
       "https://pbs.twimg.com/profile_images/1816814706000080897/uSIidPHz_400x400.png",
     handle: "@peer_rich",
